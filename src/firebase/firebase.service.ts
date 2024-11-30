b71b694caa715807d93d6c49d7ce5fd093926615
alexis dutaud <aelixs.dutaud@gmail.com>
2024-11-30 19:07:52 +0100

    wip

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { Auth } from 'firebase-admin/lib/auth/auth';
+import { Storage } from 'firebase-admin/lib/storage/storage';
 
 @Injectable()
 export class FirebaseService {
     private auth = admin.auth();
     private firestore = admin.firestore();
+    private storage = admin.storage();
 
     getAuth(): Auth {
         return this.auth;
@@ -13,5 +15,9 @@ export class FirebaseService {
 
     getFirestore() {
         return this.firestore;
+    }
+
+    getStorage(): Storage {
+        return this.storage;
     }
 }
