1354ecf35d8d83659aba62071c0aa0f7f6ce0785
alexis dutaud <aelixs.dutaud@gmail.com>
2025-01-09 15:23:37 +0100

    kafka seems to be installed, now try to use it

--- src/kafka/kafka.service.ts
+++ src/kafka/kafka.service.ts
@@ -6,6 +6,7 @@ export class KafkaService {
     constructor(@Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka) {}
 
     async sendMessage(topic: string, message: any) {
+        console.log('h');
         return this.kafkaClient.emit(topic, message);
     }
 
